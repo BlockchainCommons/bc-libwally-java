6fd55915b61684c3311f842b3a49f92b39b1d6c8
Hiếu Phạm <hieupham@bitmark.com>
2021-01-11 15:17:23 +0700

    better build scripts

--- android/scripts/build-jni.sh
+++ android/scripts/build-jni.sh
@@ -2,8 +2,13 @@
 
 set -e
 
+ROOT_DIR=$(
+  cd ..
+  pwd
+)
+
 source scripts/helper.sh
-source ../deps/libwally-core/tools/android_helpers.sh
+source "$ROOT_DIR"/deps/libwally-core/tools/android_helpers.sh
 
 echo "${JAVA_HOME:?}"
 echo "${ANDROID_NDK:?}"
@@ -13,44 +18,60 @@ TOOLCHAIN_DIR=$(android_get_build_tools_dir)
 JNI_MD_DIR="darwin"
 JNI_LIBS=(bc-libwally-address-jni bc-libwally-bip32-jni bc-libwally-bip39-jni bc-libwally-crypto-jni bc-libwally-core-jni bc-libwally-script-jni bc-libwally-tx-jni bc-libwally-psbt-jni)
 LIBWALY_CORE_FILE=libwallycore.so
+BUILD_LOG_DIR="log"
+BUILD_LOG="$BUILD_LOG_DIR/$(date +%s)-build-jni-log.txt"
+if [[ -n $1 ]]; then
+  BUILD_LOG=$1
+fi
 
 if ! is_osx; then
   JNI_MD_DIR="linux"
 fi
 
-# Install jni libs
-for ARCH in $ARCH_LIST; do
+build_jni() {
+  ARCH=$1
+  LIB=$2
+  OUT_DIR=$3
+  LIB_NAME="lib${LIB}.so"
 
-  API=19
-  if [[ $ARCH == *"64"* ]]; then
-    API=21
-  fi
+  CC=$(android_get_build_tool "$ARCH" "$TOOLCHAIN_DIR" "$API" "clang")
 
-  CC=$(android_get_build_tool "$ARCH" "$TOOLCHAIN_DIR" $API "clang")
+  SRC_ROOT_DIR=$ROOT_DIR/java/src/main
+  WALLY_ROOT_DIR=$ROOT_DIR/deps/libwally-core
 
-  OUT_DIR=app/src/main/jniLibs/$ARCH
-  mkdir -p "$OUT_DIR"
+  echo "Building $LIB_NAME..."
+  $CC -I"$JAVA_HOME/include" \
+    -I"$JAVA_HOME/include/$JNI_MD_DIR" \
+    -I"$WALLY_ROOT_DIR/include" \
+    -I"$WALLY_ROOT_DIR/src" \
+    -shared -fPIC \
+    "$SRC_ROOT_DIR/jniLibs/${LIB}.c" \
+    "$SRC_ROOT_DIR/jniLibs/jni-utils.c" \
+    "$WALLY_ROOT_DIR/src/ccan/ccan/base64/base64.c" \
+    -o \
+    "$OUT_DIR/$LIB_NAME" \
+    "$OUT_DIR/$LIBWALY_CORE_FILE"
+  echo "'$ARCH/$LIB_NAME' Done!"
+}
 
-  for LIB in "${JNI_LIBS[@]}"; do
-    LIB_NAME="lib${LIB}.so"
-    echo "Building '$LIB_NAME' for '$ARCH'..."
+(
+  exec 2>&1
+  mkdir -p ${BUILD_LOG_DIR}
+  touch "$BUILD_LOG"
 
-    SRC_ROOT_DIR=../java/src/main
-    WALLY_ROOT_DIR=../deps/libwally-core
+  for ARCH in $ARCH_LIST; do
+    API=19
+    if [[ $ARCH == *"64"* ]]; then
+      API=21
+    fi
 
-    $CC -I"$JAVA_HOME/include" \
-      -I"$JAVA_HOME/include/$JNI_MD_DIR" \
-      -I"$WALLY_ROOT_DIR/include" \
-      -I"$WALLY_ROOT_DIR/src" \
-      -shared -fPIC \
-      "$SRC_ROOT_DIR/jniLibs/${LIB}.c" \
-      "$SRC_ROOT_DIR/jniLibs/jni-utils.c" \
-      "$WALLY_ROOT_DIR/src/ccan/ccan/base64/base64.c" \
-      -o \
-      "$OUT_DIR/$LIB_NAME" \
-      "$OUT_DIR/$LIBWALY_CORE_FILE"
+    OUT_DIR=app/src/main/jniLibs/$ARCH
+    mkdir -p "$OUT_DIR"
 
-    echo "'$ARCH/$LIB_NAME' Done!"
+    for LIB in "${JNI_LIBS[@]}"; do
+      build_jni "$ARCH" "$LIB" "$OUT_DIR"
+    done
+
   done
 
-done
+) | tee -a "${BUILD_LOG}"
--- java/scripts/build-host.sh
+++ java/scripts/build-host.sh
@@ -12,26 +12,49 @@ ROOT_DIR=$(
   pwd
 )
 
-# Install libwally-core binary
-pushd "$ROOT_DIR/deps/libwally-core"
-./tools/cleanup.sh
-./tools/autogen.sh
-./configure --disable-swig-java --enable-debug
-make
-popd
-
-# Copy binary file
-echo "Copying libwally-core binary file..."
-LIBWALLY_CORE_DIR=$ROOT_DIR/deps/libwally-core/src/.libs
-LIBWALLY_CORE_FILE=libwallycore.so
-OUT_DIR=src/main/libs
-mkdir -p "$OUT_DIR"
-
-if is_osx; then
-  LIBWALLY_CORE_FILE=libwallycore.dylib
-  cp "$LIBWALLY_CORE_DIR/$LIBWALLY_CORE_FILE" "$OUT_DIR"
-else
-  find "$LIBWALLY_CORE_DIR" -name "$LIBWALLY_CORE_FILE*" -exec cp '{}' "$OUT_DIR" ';'
+BUILD_LOG_DIR="log"
+BUILD_LOG="$BUILD_LOG_DIR/$(date +%s)-build-host-log.txt"
+EXTERNAL_BUILD_LOG=$1
+if [[ -n $EXTERNAL_BUILD_LOG ]]; then
+  BUILD_LOG=$EXTERNAL_BUILD_LOG
 fi
 
-echo "Done!"
+config_libwally_core() {
+  pushd "$ROOT_DIR/deps/libwally-core"
+  ./tools/cleanup.sh
+  ./tools/autogen.sh
+  popd
+}
+
+build_libwally_core() {
+  # build libwally-core
+  pushd "$ROOT_DIR/deps/libwally-core"
+  ./configure --disable-swig-java --enable-debug
+  make
+  popd
+
+  # Copy binary file
+  LIBWALLY_CORE_DIR=$ROOT_DIR/deps/libwally-core/src/.libs
+  LIBWALLY_CORE_FILE=libwallycore.so
+  OUT_DIR=src/main/libs
+  mkdir -p "$OUT_DIR"
+
+  if is_osx; then
+    LIBWALLY_CORE_FILE=libwallycore.dylib
+    cp "$LIBWALLY_CORE_DIR/$LIBWALLY_CORE_FILE" "$OUT_DIR"
+  else
+    find "$LIBWALLY_CORE_DIR" -name "$LIBWALLY_CORE_FILE*" -exec cp '{}' "$OUT_DIR" ';'
+  fi
+
+}
+
+(
+  exec 2>&1
+  mkdir -p ${BUILD_LOG_DIR}
+  touch "$BUILD_LOG"
+
+  config_libwally_core
+
+  build_libwally_core
+
+) | tee -a "${BUILD_LOG}"
